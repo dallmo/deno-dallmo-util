26079f9eeb23f1ff1bba072fe6d31669b367e6a8
jimz-laiking-pc-flossy <jimz@dallmo.com>
2024-03-03 20:23:34 +0800

    add deps for jsr

--- etc/deps.jsr.ts
+++ etc/deps.jsr.ts
@@ -18,10 +18,10 @@ export {
 
   assertEquals
 
-} from "https://deno.land/std@0.218.2/assert/mod.ts";
+} from "jsr:@std/assert@^0.218.2";
 ////////////////////////////////////////////////////////////////////////////////
-export * as array  from "https://deno.land/x/dallmo_util_array@v1.2.0/mod.ts"; // published deno module
-export * as math   from "https://deno.land/x/dallmo_util_math@v1.1.0/mod.ts"; // published deno module
-export * as string from "https://deno.land/x/dallmo_util_string@v1.2.0/mod.ts"; // published deno module
-export * as yaml   from "https://deno.land/x/dallmo_util_yaml@v1.5.2/mod.ts"; // published deno module
+export * as array  from "jsr:@dallmo/util-array@^1.2.0";
+export * as math   from "jsr:@dallmo/util-math@^1.1.0";
+export * as string from "jsr:@dallmo/util-string@^1.2.0";
+export * as yaml   from "jsr:@dallmo/util-yaml@^1.5.2";
 ////////////////////////////////////////////////////////////////////////////////
--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,5 @@
 // deno test methods
-import { assertEquals } from "../deps.ts";
+import { assertEquals } from "../etc/deps.ts";
 
 // the methods of this module to be tested
 import * as dallmo_util from "../mod.ts";
@@ -23,18 +23,6 @@ Deno.test( "test reaching dallmo-util-string", () => {
 
 }); // Deno.test
 /////////////////////////////////////////////////////////////
-Deno.test( "test reaching dallmo-util-timer", () => {
-
-  assertEquals( dallmo_util.test.timer(), "ok");
-
-}); // Deno.test
-/////////////////////////////////////////////////////////////
-Deno.test( "test reaching dallmo-util-timestamp", () => {
-
-  assertEquals( dallmo_util.test.timestamp(), "ok");
-
-}); // Deno.test
-/////////////////////////////////////////////////////////////
 Deno.test( "test reaching dallmo-util-yaml", () => {
 
   assertEquals( dallmo_util.test.yaml(), "ok");
@@ -46,11 +34,11 @@ Deno.test( "test child module string : cap / capitalize", () => {
   const input_string: string = "abcde";
   let result: string;
 
-  result = dallmo_util.string.cap( input_string );
+  result = dallmo_util.string.case_title( input_string );
   assertEquals( result, "Abcde");
 
-  result = dallmo_util.string.capitalize( input_string );
-  assertEquals( result, "Abcde");  
+  result = dallmo_util.string.case_upper( input_string );
+  assertEquals( result, "ABCDE");  
 
 }); // Deno.test
 /////////////////////////////////////////////////////////////
